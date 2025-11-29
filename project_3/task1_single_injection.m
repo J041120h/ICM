02e1ab6be2c18ac540ce99d66598ae6557bcaaf0
Harry Jiang <hjiang55@jh.edu>
2025-11-29 16:49:38 -0500

    a complete workable version of project 3 part A

--- project_3/task1_single_injection.m
+++ project_3/task1_single_injection.m
@@ -1,181 +1,167 @@
 function task1_single_injection
-% Task 1 – Single 10 mg/kg IV injection of bevacizumab
-% Uses VEGFAbeqns.m (must be in same folder, UNMODIFIED).
-% Produces a 3x2 panel figure and saves it as task1_single_injection.png.
-%
-% Panels:
-%   Row 1: Free VEGF (no extravasation vs with extravasation)
-%   Row 2: Free antibody
-%   Row 3: VEGF–antibody complex
 
-    clearvars -except task1_single_injection;  %#ok<CLVAR>
-    clc;
+clearvars -except task1_single_injection;
+clc;
 
-    %% Index array (must match VEGFAbeqns.m)
-    n.VEGF_b        = 1;
-    n.VEGFR2_b      = 2;
-    n.VEGFVEGFR2_b  = 3;
-    n.Ab_b          = 4;
-    n.VEGFAb_b      = 5;
+n.VEGF_b        = 1;
+n.VEGFR2_b      = 2;
+n.VEGFVEGFR2_b  = 3;
+n.Ab_b          = 4;
+n.VEGFAb_b      = 5;
+n.VEGF_t        = 6;
+n.VEGFR2_t      = 7;
+n.VEGFVEGFR2_t  = 8;
+n.Ab_t          = 9;
+n.VEGFAb_t      = 10;
+n.VEGF_r        = 11;
+n.VEGFR2_r      = 12;
+n.VEGFVEGFR2_r  = 13;
+n.Ab_r          = 14;
+n.VEGFAb_r      = 15;
 
-    n.VEGF_t        = 6;
-    n.VEGFR2_t      = 7;
-    n.VEGFVEGFR2_t  = 8;
-    n.Ab_t          = 9;
-    n.VEGFAb_t      = 10;
+p.Vol_b = 5 * 0.60;
+p.Vol_t = 1 * 0.611;
+p.Vol_r = 40 * 0.0816;
 
-    n.VEGF_r        = 11;
-    n.VEGFR2_r      = 12;
-    n.VEGFVEGFR2_r  = 13;
-    n.Ab_r          = 14;
-    n.VEGFAb_r      = 15;
+p.kcl_V   = 0.0648;
+p.kcl_VA  = 2.2e-5;
+p.kcl_A   = 2.2e-5;
 
-    %% Parameters (from starter code / assignment)
-    % Compartment volumes (L)
-    p.Vol_b = 5 * 0.60;    % blood plasma
-    p.Vol_t = 1 * 0.611;   % tumor interstitial
-    p.Vol_r = 40 * 0.0816; % rest-of-body interstitial
+p.kintR2  = 0.0168;
+p.kintVR2 = 0.0168;
 
-    % Clearance and internalization (min^-1)
-    p.kcl_V   = 0.0648;    % free VEGF clearance
-    p.kcl_VA  = 2.2e-5;    % VEGF–Ab clearance
-    p.kcl_A   = 2.2e-5;    % Ab clearance
+p.VEGFprod_b   = 0;
+p.VEGFprod_t   = 3;
+p.VEGFprod_r   = 10;
 
-    p.kintR2  = 0.0168;    % VEGFR2 internalization
-    p.kintVR2 = 0.0168;    % VEGF–VEGFR2 internalization
+p.VEGFR2prod_b = 0;
+p.VEGFR2prod_t = 2.85e2 * p.kintR2;
+p.VEGFR2prod_r = 2.2e3  * p.kintR2;
 
-    % Production (pM/min)
-    p.VEGFprod_b   = 0;
-    p.VEGFprod_t   = 3;
-    p.VEGFprod_r   = 10;
+p.konVR  = 6e-4;
+p.koffVR = 0.06;
+p.konVA  = 5.52e-6;
+p.koffVA = 0.012;
 
-    p.VEGFR2prod_b = 0;
-    p.VEGFR2prod_t = 2.85e2 * p.kintR2;   % 285 pM * kint
-    p.VEGFR2prod_r = 2.2e3  * p.kintR2;   % 2200 pM * kint
+p.k_bt = 4.12e-4;
+p.k_br = 3.19e-3;
+p.k_tb = 4.12e-4;
+p.k_rb = 3.19e-3 + 6.15e-4;
 
-    % Binding kinetics (kon: pM^-1 min^-1, koff: min^-1)
-    p.konVR  = 6e-4;
-    p.koffVR = 0.06;
+p.AbEx = 1;
 
-    p.konVA  = 5.52e-6;
-    p.koffVA = 0.012;
+dose_mg_total   = 10 * 70;
+MW_mg_per_mol   = 150000000;
+dose_mol_total  = dose_mg_total / MW_mg_per_mol;
+dose_pmol_total = dose_mol_total * 1e12;
+dose_conc_pM    = dose_pmol_total / p.Vol_b;
 
-    % Transport rates (min^-1)
-    p.k_bt = 4.12e-4;               % blood -> tumor
-    p.k_br = 3.19e-3;               % blood -> rest
-    p.k_tb = 4.12e-4;               % tumor -> blood
-    p.k_rb = 3.19e-3 + 6.15e-4;     % rest -> blood (includes lymphatics)
+options = odeset('MaxStep', 5e-2, ...
+                 'AbsTol', 1e-5, ...
+                 'RelTol', 1e-5, ...
+                 'InitialStep', 1e-2);
 
-    % Default: Ab transport allowed (will override for each case)
-    p.AbEx = 1;
+y0      = zeros(15,1);
+sstime  = 60 * 24 * 10;
+tspanSS = 0:1:sstime;
 
-    %% Antibody dose (single IV bolus)
-    % 10 mg/kg * 70 kg = 700 mg total; MW = 150 kDa = 150,000,000 mg/mol
-    dose_mg_total   = 10 * 70;
-    MW_mg_per_mol   = 150000000;
-    dose_mol_total  = dose_mg_total / MW_mg_per_mol;
-    dose_pmol_total = dose_mol_total * 1e12;    % pmol
-    dose_conc_pM    = dose_pmol_total / p.Vol_b;  % pM increase in blood
+[~, Yss] = ode45(@VEGFAbeqns, tspanSS, y0, options, p, n);
+y_ss = Yss(end, :).';
 
-    %% ODE solver options
-    options = odeset('MaxStep', 5e-2, ...
-                     'AbsTol', 1e-5, ...
-                     'RelTol', 1e-5, ...
-                     'InitialStep', 1e-2);
+endtime = 60 * 24 * 21;
+tspan   = 0:1:endtime;
 
-    %% 1) Run to steady state with NO antibody
-    y0      = zeros(15,1);
-    sstime  = 60 * 24 * 10;          % 10 days in minutes
-    tspanSS = 0:1:sstime;
+p_noEx          = p;
+p_noEx.AbEx     = 0;
+y0_noEx         = y_ss;
+y0_noEx(n.Ab_b) = y0_noEx(n.Ab_b) + dose_conc_pM;
+[T_noEx, Y_noEx] = ode45(@VEGFAbeqns, tspan, y0_noEx, options, p_noEx, n);
 
-    % Antibody-related parameters irrelevant here (Ab=0)
-    [Tss, Yss] = ode45(@VEGFAbeqns, tspanSS, y0, options, p, n); %#ok<ASGLU>
-    y_ss = Yss(end, :).';   % steady-state vector
+p_Ex          = p;
+p_Ex.AbEx     = 1;
+y0_Ex         = y_ss;
+y0_Ex(n.Ab_b) = y0_Ex(n.Ab_b) + dose_conc_pM;
+[T_Ex, Y_Ex] = ode45(@VEGFAbeqns, tspan, y0_Ex, options, p_Ex, n);
 
-    %% 2) Post-dose simulations (21 days after dose)
-    endtime = 60 * 24 * 21;          % 21 days in minutes
-    tspan   = 0:1:endtime;
+t_noEx_days = T_noEx / (60*24);
+t_Ex_days   = T_Ex   / (60*24);
 
-    % ---------- Case A: NO antibody extravasation ----------
-    p_noEx       = p;
-    p_noEx.AbEx  = 0;                       % key: no extravasation
-    y0_noEx      = y_ss;
-    y0_noEx(n.Ab_b) = y0_noEx(n.Ab_b) + dose_conc_pM;
+Ab_noEx_uM_r = Y_noEx(:,n.Ab_r) / 1e6;
+Ab_noEx_uM_b = Y_noEx(:,n.Ab_b) / 1e6;
+Ab_noEx_uM_t = Y_noEx(:,n.Ab_t) / 1e6;
 
-    [T_noEx, Y_noEx] = ode45(@VEGFAbeqns, tspan, y0_noEx, options, p_noEx, n);
+Ab_Ex_uM_r = Y_Ex(:,n.Ab_r) / 1e6;
+Ab_Ex_uM_b = Y_Ex(:,n.Ab_b) / 1e6;
+Ab_Ex_uM_t = Y_Ex(:,n.Ab_t) / 1e6;
 
-    % ---------- Case B: WITH antibody extravasation ----------
-    p_Ex       = p;
-    p_Ex.AbEx  = 1;                         % key: extravasation allowed
-    y0_Ex      = y_ss;
-    y0_Ex(n.Ab_b) = y0_Ex(n.Ab_b) + dose_conc_pM;
+C_noEx_nM_r = Y_noEx(:,n.VEGFAb_r) / 1e3;
+C_noEx_nM_b = Y_noEx(:,n.VEGFAb_b) / 1e3;
+C_noEx_nM_t = Y_noEx(:,n.VEGFAb_t) / 1e3;
 
-    [T_Ex, Y_Ex] = ode45(@VEGFAbeqns, tspan, y0_Ex, options, p_Ex, n);
+C_Ex_nM_r = Y_Ex(:,n.VEGFAb_r) / 1e3;
+C_Ex_nM_b = Y_Ex(:,n.VEGFAb_b) / 1e3;
+C_Ex_nM_t = Y_Ex(:,n.VEGFAb_t) / 1e3;
 
-    % Convert time to days for plotting
-    t_noEx_days = T_noEx / (60*24);
-    t_Ex_days   = T_Ex   / (60*24);
+ymax_complex_Ex = max(C_Ex_nM_t) * 1.1;
 
-    %% 3) Plot (but do NOT show window) and save locally
-    fig = figure('Visible','off');
-    set(fig, 'Position', [100 100 1200 800]);
+fig = figure('Visible','off');
+set(fig, 'Position', [100 100 1200 800]);
+lw = 2;
 
-    lw = 2;
+% Row 1: Free VEGF (pM)
+% --- FIX: zoom y-limits so blood 0–5 pM drop/recovery is visible ---
+subplot(3,2,1);
+plot(t_noEx_days, Y_noEx(:,n.VEGF_r), '-',  ...
+     t_noEx_days, Y_noEx(:,n.VEGF_b), '--', ...
+     t_noEx_days, Y_noEx(:,n.VEGF_t), ':',  'LineWidth', lw);
+title('Free VEGF – NO Ab extravasation');
+xlabel('Time (days)'); ylabel('Free VEGF (pM)');
+legend({'Normal tissue','Blood','Tumor'}, 'Location','best');
+ylim([0 200]);
 
-    % Row 1: Free VEGF
-    subplot(3,2,1);
-    plot(t_noEx_days, Y_noEx(:,n.VEGF_r), '-',  ...
-         t_noEx_days, Y_noEx(:,n.VEGF_b), '--', ...
-         t_noEx_days, Y_noEx(:,n.VEGF_t), ':',  'LineWidth', lw);
-    title('Free VEGF – NO Ab extravasation');
-    xlabel('Time (days)'); ylabel('[VEGF] (pM)');
-    legend({'Rest of body','Blood','Tumor'}, 'Location','best');
+subplot(3,2,2);
+plot(t_Ex_days, Y_Ex(:,n.VEGF_r), '-', ...
+     t_Ex_days, Y_Ex(:,n.VEGF_b), '--', ...
+     t_Ex_days, Y_Ex(:,n.VEGF_t), ':', 'LineWidth', lw);
+title('Free VEGF – WITH Ab extravasation');
+xlabel('Time (days)'); ylabel('[VEGF] (pM)');
+legend({'Normal tissue','Blood','Tumor'}, 'Location','best');
+ylim([0 220]);
 
-    subplot(3,2,2);
-    plot(t_Ex_days, Y_Ex(:,n.VEGF_r), '-',  ...
-         t_Ex_days, Y_Ex(:,n.VEGF_b), '--', ...
-         t_Ex_days, Y_Ex(:,n.VEGF_t), ':',  'LineWidth', lw);
-    title('Free VEGF – WITH Ab extravasation');
-    xlabel('Time (days)'); ylabel('[VEGF] (pM)');
-    legend({'Rest of body','Blood','Tumor'}, 'Location','best');
+subplot(3,2,3);
+plot(t_noEx_days, Ab_noEx_uM_r, '-', ...
+     t_noEx_days, Ab_noEx_uM_b, '--', ...
+     t_noEx_days, Ab_noEx_uM_t, ':', 'LineWidth', lw);
+title('Free anti-VEGF – NO Ab extravasation');
+xlabel('Time (days)'); ylabel('Free anti-VEGF (\muM)');
+legend({'Normal tissue','Blood','Tumor'}, 'Location','best');
 
-    % Row 2: Free antibody
-    subplot(3,2,3);
-    plot(t_noEx_days, Y_noEx(:,n.Ab_r), '-',  ...
-         t_noEx_days, Y_noEx(:,n.Ab_b), '--', ...
-         t_noEx_days, Y_noEx(:,n.Ab_t), ':',  'LineWidth', lw);
-    title('Free antibody – NO Ab extravasation');
-    xlabel('Time (days)'); ylabel('[Ab] (pM)');
-    legend({'Rest of body','Blood','Tumor'}, 'Location','best');
+subplot(3,2,4);
+plot(t_Ex_days, Ab_Ex_uM_r, '-', ...
+     t_Ex_days, Ab_Ex_uM_b, '--', ...
+     t_Ex_days, Ab_Ex_uM_t, ':', 'LineWidth', lw);
+title('Free anti-VEGF – WITH Ab extravasation');
+xlabel('Time (days)'); ylabel('Free anti-VEGF (\muM)');
+legend({'Normal tissue','Blood','Tumor'}, 'Location','best');
 
-    subplot(3,2,4);
-    plot(t_Ex_days, Y_Ex(:,n.Ab_r), '-',  ...
-         t_Ex_days, Y_Ex(:,n.Ab_b), '--', ...
-         t_Ex_days, Y_Ex(:,n.Ab_t), ':',  'LineWidth', lw);
-    title('Free antibody – WITH Ab extravasation');
-    xlabel('Time (days)'); ylabel('[Ab] (pM)');
-    legend({'Rest of body','Blood','Tumor'}, 'Location','best');
+subplot(3,2,5);
+plot(t_noEx_days, C_noEx_nM_r, '-', ...
+     t_noEx_days, C_noEx_nM_b, '--', ...
+     t_noEx_days, C_noEx_nM_t, ':', 'LineWidth', lw);
+title('VEGF–anti-VEGF complex – NO Ab extravasation');
+xlabel('Time (days)'); ylabel('[VEGF–anti-VEGF] (nM)');
+legend({'Normal tissue','Blood','Tumor'}, 'Location','best');
 
-    % Row 3: VEGF–Ab complex
-    subplot(3,2,5);
-    plot(t_noEx_days, Y_noEx(:,n.VEGFAb_r), '-',  ...
-         t_noEx_days, Y_noEx(:,n.VEGFAb_b), '--', ...
-         t_noEx_days, Y_noEx(:,n.VEGFAb_t), ':',  'LineWidth', lw);
-    title('VEGF–Ab complex – NO Ab extravasation');
-    xlabel('Time (days)'); ylabel('[VEGF–Ab] (pM)');
-    legend({'Rest of body','Blood','Tumor'}, 'Location','best');
+subplot(3,2,6);
+plot(t_Ex_days, C_Ex_nM_r, '-', ...
+     t_Ex_days, C_Ex_nM_b, '--', ...
+     t_Ex_days, C_Ex_nM_t, ':', 'LineWidth', lw);
+title('VEGF–anti-VEGF complex – WITH Ab extravasation');
+xlabel('Time (days)'); ylabel('[VEGF–anti-VEGF] (nM)');
+legend({'Normal tissue','Blood','Tumor'}, 'Location','best');
+ylim([0 ymax_complex_Ex]);
 
-    subplot(3,2,6);
-    plot(t_Ex_days, Y_Ex(:,n.VEGFAb_r), '-',  ...
-         t_Ex_days, Y_Ex(:,n.VEGFAb_b), '--', ...
-         t_Ex_days, Y_Ex(:,n.VEGFAb_t), ':',  'LineWidth', lw);
-    title('VEGF–Ab complex – WITH Ab extravasation');
-    xlabel('Time (days)'); ylabel('[VEGF–Ab] (pM)');
-    legend({'Rest of body','Blood','Tumor'}, 'Location','best');
-
-    sgtitle('Task 1 – Single 10 mg/kg IV injection of bevacizumab');
-
-    % Save figure locally and close
-    saveas(fig, 'task1_single_injection.png');
-    close(fig);
+sgtitle('Task 1 – Single 10 mg/kg IV injection of bevacizumab');
+saveas(fig, 'task1_single_injection.png');
+close(fig);
 end
--- project_3/task2_metronomic.m
+++ project_3/task2_metronomic.m
@@ -1,18 +1,8 @@
-
 function task2_metronomic
-% Task 2 – Metronomic therapy (1 mg/kg IV daily for 10 days)
-% Recreates analogs of Stefanini Figs 1C/D, S1C/D, S2C/D using the
-% simplified VEGF–VEGFR2–Ab model in VEGFAbeqns.m.
-%
-% This code is structurally very similar to task1_single_injection and
-% reuses the same parameter values and steady-state procedure.
-%
-% Output: saves a 3x2 panel figure as "task2_metronomic.png".
 
-    clearvars -except task2_metronomic;  %#ok<CLVAR>
+    clearvars -except task2_metronomic;
     clc;
 
-    %% Index array (must match VEGFAbeqns.m)
     n.VEGF_b        = 1;
     n.VEGFR2_b      = 2;
     n.VEGFVEGFR2_b  = 3;
@@ -31,13 +21,10 @@ function task2_metronomic
     n.Ab_r          = 14;
     n.VEGFAb_r      = 15;
 
-    %% Parameters (same as Task 1)
-    % Compartment volumes (L)
     p.Vol_b = 5 * 0.60;
     p.Vol_t = 1 * 0.611;
     p.Vol_r = 40 * 0.0816;
 
-    % Clearance and internalization (min^-1)
     p.kcl_V   = 0.0648;
     p.kcl_VA  = 2.2e-5;
     p.kcl_A   = 2.2e-5;
@@ -45,7 +32,6 @@ function task2_metronomic
     p.kintR2  = 0.0168;
     p.kintVR2 = 0.0168;
 
-    % Production (pM/min)
     p.VEGFprod_b   = 0;
     p.VEGFprod_t   = 3;
     p.VEGFprod_r   = 10;
@@ -54,69 +40,52 @@ function task2_metronomic
     p.VEGFR2prod_t = 2.85e2 * p.kintR2;
     p.VEGFR2prod_r = 2.2e3  * p.kintR2;
 
-    % Binding kinetics (kon: pM^-1 min^-1, koff: min^-1)
     p.konVR  = 6e-4;
     p.koffVR = 0.06;
 
     p.konVA  = 5.52e-6;
     p.koffVA = 0.012;
 
-    % Transport rates (min^-1)
     p.k_bt = 4.12e-4;
     p.k_br = 3.19e-3;
     p.k_tb = 4.12e-4;
     p.k_rb = 3.19e-3 + 6.15e-4;
 
-    % Ab extravasation flag (0 or 1) will be set per scenario
     p.AbEx = 1;
 
-    %% Antibody dose
-    % First compute the 10 mg/kg dose as in Task 1
-    dose10_mg_total   = 10 * 70;            % mg
-    MW_mg_per_mol     = 150000000;         % 150 kDa
+    dose10_mg_total   = 10 * 70;
+    MW_mg_per_mol     = 150000000;
     dose10_mol_total  = dose10_mg_total / MW_mg_per_mol;
     dose10_pmol_total = dose10_mol_total * 1e12;
-    dose10_conc_pM    = dose10_pmol_total / p.Vol_b;   % pM
+    dose10_conc_pM    = dose10_pmol_total / p.Vol_b;
 
-    % For metronomic therapy: 1 mg/kg daily (1/10 of 10 mg/kg)
     dose1_conc_pM = dose10_conc_pM / 10;
 
-    %% ODE solver options
     options = odeset('MaxStep', 5e-2, ...
                      'AbsTol', 1e-5, ...
                      'RelTol', 1e-5, ...
                      'InitialStep', 1e-2);
 
-    %% 1) Run to steady state with NO antibody
     y0      = zeros(15,1);
-    sstime  = 60 * 24 * 10;          % 10 days in minutes
+    sstime  = 60 * 24 * 10;
     tspanSS = 0:1:sstime;
 
     [~, Yss] = ode45(@VEGFAbeqns, tspanSS, y0, options, p, n);
-    y_ss = Yss(end, :).';            % steady-state vector
+    y_ss = Yss(end, :).';
 
-    %% 2) Metronomic dosing design
-    n_days_dose = 10;                % 10 daily doses
-    total_days  = 30;                % simulate 30 days total, like Fig 1C/D
+    n_days_dose = 10;
+    total_days  = 30;
     minutes_per_day = 24 * 60;
 
-    %% ---- Helper anonymous to run one metronomic scenario ----
     function [T_all, Y_all] = run_metronomic_for_p(p_local)
-        % Start from steady state
         y_curr  = y_ss;
         t_curr  = 0;
         T_all   = [];
         Y_all   = [];
-
-        % 10 days of daily dosing
         for d = 1:n_days_dose
-            % Add daily dose at the beginning of each day
             y_curr(n.Ab_b) = y_curr(n.Ab_b) + dose1_conc_pM;
-
             tspan_day = t_curr:1:(t_curr + minutes_per_day);
             [T_day, Y_day] = ode45(@VEGFAbeqns, tspan_day, y_curr, options, p_local, n);
-
-            % Append (avoid duplicating the starting time point)
             if isempty(T_all)
                 T_all = T_day;
                 Y_all = Y_day;
@@ -124,45 +93,35 @@ function task2_metronomic
                 T_all = [T_all; T_day(2:end)];
                 Y_all = [Y_all; Y_day(2:end,:)];
             end
-
-            % Update for next loop
             t_curr = t_curr + minutes_per_day;
             y_curr = Y_day(end, :).';
         end
-
-        % After last dose, simulate until total_days
         final_time = total_days * minutes_per_day;
         if t_curr < final_time
             tspan_tail = t_curr:1:final_time;
             [T_tail, Y_tail] = ode45(@VEGFAbeqns, tspan_tail, y_curr, options, p_local, n);
-
             T_all = [T_all; T_tail(2:end)];
             Y_all = [Y_all; Y_tail(2:end,:)];
         end
     end
 
-    %% 3) Scenario A – NO Ab extravasation
     p_noEx      = p;
     p_noEx.AbEx = 0;
 
     [T_noEx, Y_noEx] = run_metronomic_for_p(p_noEx);
 
-    %% 4) Scenario B – WITH Ab extravasation
     p_Ex      = p;
     p_Ex.AbEx = 1;
 
     [T_Ex, Y_Ex] = run_metronomic_for_p(p_Ex);
 
-    % Convert to days
     t_noEx_days = T_noEx / (60*24);
     t_Ex_days   = T_Ex   / (60*24);
 
-    %% 5) Plot (invisible) and save
     fig = figure('Visible','off');
     set(fig, 'Position', [100 100 1200 800]);
     lw = 2;
 
-    % Row 1: Free VEGF
     subplot(3,2,1);
     plot(t_noEx_days, Y_noEx(:,n.VEGF_r), '-',  ...
          t_noEx_days, Y_noEx(:,n.VEGF_b), '--', ...
@@ -179,7 +138,6 @@ function task2_metronomic
     xlabel('Time (days)'); ylabel('[VEGF] (pM)');
     legend({'Rest of body','Blood','Tumor'}, 'Location','best');
 
-    % Row 2: Free antibody
     subplot(3,2,3);
     plot(t_noEx_days, Y_noEx(:,n.Ab_r), '-',  ...
          t_noEx_days, Y_noEx(:,n.Ab_b), '--', ...
@@ -196,7 +154,6 @@ function task2_metronomic
     xlabel('Time (days)'); ylabel('[Ab] (pM)');
     legend({'Rest of body','Blood','Tumor'}, 'Location','best');
 
-    % Row 3: VEGF–Ab complex
     subplot(3,2,5);
     plot(t_noEx_days, Y_noEx(:,n.VEGFAb_r), '-',  ...
          t_noEx_days, Y_noEx(:,n.VEGFAb_b), '--', ...
@@ -215,7 +172,6 @@ function task2_metronomic
 
     sgtitle('Task 2 – Metronomic therapy (1 mg/kg daily × 10 days)');
 
-    % Save and close
     saveas(fig, 'task2_metronomic.png');
     close(fig);
 end
